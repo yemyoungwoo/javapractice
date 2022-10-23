ff22052919f1f4d949dc4243580a447966fcdff3
yemyoungwoo <tkddlektks@gmail.com>
2022-10-23 19:32:05 +0900

    자바로 계산기 만들기

---
+++ src/Calculator.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class Calculator extends JFrame {
+	
+	public Calculator() {
+		setTitle("계산기");
+		setVisible(true);
+		setSize(300, 370);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		new Calculator();
+	}
+}
